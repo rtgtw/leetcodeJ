073a39c41aa1fc5c1fb68bb40e5955e41e011317
Remi Olateru <remiolateru@gmail.com>
2024-09-15 19:14:02 -0400

    Delete middle Node, superset of middleNode

---
+++ src/Leetcode/TwoPointers/FastAndSlow/DeleteMiddleNode2095.java
@@ -0,0 +1,34 @@
+package Leetcode.TwoPointers.FastAndSlow;
+
+public class DeleteMiddleNode2095 {
+
+
+    public ListNode deleteMiddle(ListNode head) {
+
+        //initialize two pointers, fast and slow both starting at head
+        //Slow trail is going to be following slow, this accomplishes n/2
+        ListNode slowTrail = null;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        //Iterate through the linked list while fast isnt null, time complexity o(n) iterating thru node
+        //The reason why we need to do fast and fast's next is because we do not want to iterate to a null value
+        //you cannot dereference a null value
+        while(fast != null && fast.next != null){
+            slowTrail = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //now we connect slowTrail with slow.next
+        //if slowtrail is still null
+        //fast.next was null meaning, theres only 1 node, just return null (at first i had head but its null)
+        if(slowTrail == null){
+            return slowTrail;
+        }
+
+        //"deleting" the node, just replacing it, garbage collector in java deletes the memory allocation
+        slowTrail.next = slow.next;
+        return head;
+    }
+}
--- src/Leetcode/TwoPointers/FastAndSlow/MiddleOfLinkedList876.java
+++ src/Leetcode/TwoPointers/FastAndSlow/MiddleOfLinkedList876.java
@@ -26,6 +26,31 @@ public class MiddleOfLinkedList876 {
         return slow;
     }
 
+    public ListNode middleNodeN(ListNode head){
+        //return the middle node
+        //initialize two pointers, fast and slow both starting at head
+        ListNode slow = head;
+        ListNode fast = head;
+
+        //Iterate through the linked list while fast isnt null, time complexity o(n) iterating thru node
+        //The reason why we need to do fast and fast's next is because we do not want to iterate to a null value
+        //you cannot dereference a null value
+        while(fast != null && fast.next != null){
+            //Move fast, and if fast is already at null just return slow
+            fast = fast.next.next;
+
+            if(fast != null && fast.next != null){
+                slow = slow.next;
+            }else{
+                return slow;
+            }
+        }
+
+        //Now all we have to do is return slow
+
+        return slow;
+    }
+
 
     public static void main(String args[]){
         ListNode head = new ListNode(1);
@@ -35,11 +60,12 @@ public class MiddleOfLinkedList876 {
         head.next.next.next.next = new ListNode(5);
         head.next.next.next.next.next = new ListNode(6);
         head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
 
         MiddleOfLinkedList876 sol = new MiddleOfLinkedList876();
 
         //show the value of the returned node
-        System.out.println(sol.middleNode(head).val);
+        System.out.println(sol.middleNodeN(head).val);
 
         //Time complexity o(N) just iterating through a linkedList
         //Space complexity o(1) no extra space
