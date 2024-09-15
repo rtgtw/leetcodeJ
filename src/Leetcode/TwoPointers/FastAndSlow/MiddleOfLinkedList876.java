8c163dc04d019b0dbb456fb4db8a6e0cd1366a5f
Remi Olateru <remiolateru@gmail.com>
2024-09-15 18:08:04 -0400

    Middle of linked list, i liked this one, simple and very effective, to find the middle of something just use two pointers

---
+++ src/Leetcode/TwoPointers/FastAndSlow/MiddleOfLinkedList876.java
@@ -0,0 +1,50 @@
+package Leetcode.TwoPointers.FastAndSlow;
+
+
+//To find the middle of the linked list you just use a fast and slow pointer
+//1 and 2 speed
+
+public class MiddleOfLinkedList876 {
+
+
+    public ListNode middleNode(ListNode head){
+        //return the middle node
+        //initialize two pointers, fast and slow both starting at head
+        ListNode slow = head;
+        ListNode fast = head;
+
+        //Iterate through the linked list while fast isnt null, time complexity o(n) iterating thru node
+        //The reason why we need to do fast and fast's next is because we do not want to iterate to a null value
+        //you cannot dereference a null value
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //Now all we have to do is return slow
+
+        return slow;
+    }
+
+
+    public static void main(String args[]){
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+
+        MiddleOfLinkedList876 sol = new MiddleOfLinkedList876();
+
+        //show the value of the returned node
+        System.out.println(sol.middleNode(head).val);
+
+        //Time complexity o(N) just iterating through a linkedList
+        //Space complexity o(1) no extra space
+    }
+
+
+
+}
